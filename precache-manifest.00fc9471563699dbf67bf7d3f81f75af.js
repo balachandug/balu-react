1015d3b15b32f1e2247683a071a50851ef1bdade
Balu <balachandu.gillella@sap.com>
2020-05-03 17:36:18 +0530

    Updates

---
+++ precache-manifest.00fc9471563699dbf67bf7d3f81f75af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670d9d7c3b21f68e5d1260cbc6137921",
+    "url": "/balu-react/index.html"
+  },
+  {
+    "revision": "098985a98f9316630242",
+    "url": "/balu-react/static/css/main.f378ae78.chunk.css"
+  },
+  {
+    "revision": "59bbd5136349456ffec3",
+    "url": "/balu-react/static/js/2.ea1a6e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/balu-react/static/js/2.ea1a6e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098985a98f9316630242",
+    "url": "/balu-react/static/js/main.f1e5c5ea.chunk.js"
+  },
+  {
+    "revision": "56851725fd0bc761bdf5",
+    "url": "/balu-react/static/js/runtime-main.35a87438.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/balu-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
